41c0e916a64c87d9d6b7210c35157a85fa8e1eea
paragjaiswal82 <89028091+paragjaiswal82@users.noreply.github.com>
2021-08-18 17:43:15 +0530

    Update SpringSecurityExampleApplication.java
    added resttemplate class

--- src/main/java/com/example/demo/SpringSecurityExampleApplication.java
+++ src/main/java/com/example/demo/SpringSecurityExampleApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringSecurityExampleApplication {
@@ -9,5 +11,11 @@ public class SpringSecurityExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityExampleApplication.class, args);
 	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
+	}
 
 }
+
